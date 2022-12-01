05394bb481b1c9e49706876c4c657c85125d7624
meipp <meipp@users.noreply.github.com>
2022-12-01 14:30:37 +0100

    Restructure /exec

--- modules/exec.py
+++ modules/exec.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message
 import os
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, TimeoutExpired
 
 from bot.bot import on, listen_on_change
 from bot.util import parseArgs
@@ -10,16 +10,20 @@ whitelist = os.environ["EXEC_WHITELIST"].split(":")
 @on(command="exec")
 async def command_exec(message: Message, *_):
     print(message.from_user.first_name, message.from_user.id)
+    if str(message.from_user.id) not in whitelist:
+        raise RuntimeError("User is not whitelisted")
 
     async def exec(message: Message):
-        if str(message.from_user.id) not in whitelist:
-            raise RuntimeError("User is not whitelisted")
-
         args, kwargs = parseArgs(message)
         command = args
+        timeout = 5
 
         p = Popen(command, stdout=PIPE, stderr=PIPE, encoding="utf-8")
-        stdout, stderr = p.communicate()
+        try:
+            stdout, stderr = p.communicate(timeout=timeout)
+        except TimeoutExpired:
+            p.kill()
+            raise TimeoutExpired(command, timeout)
 
         if p.returncode == 0:
             return stdout
