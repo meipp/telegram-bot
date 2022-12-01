c1751c53b223694ff4817329d223d77cc0b4b9b4
meipp <meipp@users.noreply.github.com>
2022-12-01 14:30:47 +0100

    Add /shell

--- modules/exec.py
+++ modules/exec.py
@@ -1,6 +1,6 @@
 from aiogram.types import Message
 import os
-from subprocess import Popen, PIPE, TimeoutExpired
+from subprocess import run, Popen, PIPE, TimeoutExpired
 
 from bot.bot import on, listen_on_change
 from bot.util import parseArgs
@@ -29,6 +29,28 @@ async def command_exec(message: Message, *_):
             return stdout
         else:
             return f"status: {p.returncode}\nstdout: {stdout}\nstderr: {stderr}"
+
+
+    await listen_on_change(message).then(exec).send()
+
+@on(command="shell")
+async def command_shell(message: Message, *_):
+    print(message.from_user.first_name, message.from_user.id)
+    if str(message.from_user.id) not in whitelist:
+        raise RuntimeError("User is not whitelisted")
+
+    async def exec(message: Message):
+        args, kwargs = parseArgs(message)
+        command = message.get_args()
+        timeout = 5
+
+        p = run(command, shell=True, capture_output=True, encoding="utf-8", timeout=timeout)
+        print("stdout", p.stdout)
+
+        if p.returncode == 0:
+            return p.stdout
+        else:
+            return f"status: {p.returncode}\nstdout: {p.stdout}\nstderr: {p.stderr}"
 
 
     await listen_on_change(message).then(exec).send()
